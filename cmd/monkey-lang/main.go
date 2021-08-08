62f7b105132bec5fe18f99f905052a81e0d92a62
Hideaki Tarumi <hideakit803@gmail.com>
2021-08-08 15:51:36 +0900

    replの追加

--- cmd/monkey-lang/main.go
+++ cmd/monkey-lang/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/hideA88/monkey-lang/pkg/repl"
+	"os"
 	"os/user"
 )
 
@@ -13,4 +15,5 @@ func main() {
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -1,1 +1,33 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/hideA88/monkey-lang/pkg/lexer"
+	"github.com/hideA88/monkey-lang/pkg/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		_, err := fmt.Fprint(out, PROMPT)
+		if err != nil {
+			println(err)
+			return
+		}
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
